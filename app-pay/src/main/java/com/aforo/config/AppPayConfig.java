8313b608430b67eb478beb08286c3c475d7015e4
DaniBonica001 <danielabc_0106@hotmail.com>
2024-02-12 15:01:32 -0500

    Payments bigger than the amount are not allowed

---
+++ app-pay/src/main/java/com/aforo/config/AppPayConfig.java
@@ -0,0 +1,13 @@
+package com.aforo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+@Configuration
+public class AppPayConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
